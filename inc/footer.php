97c00dcfdd68ced424dcf06a7dd9a99aea05c0bd
Carlos Doe <johndoe@example.com>
2016-02-25 22:26:48 +0100

    Copia de la web antigua

---
+++ inc/footer.php
@@ -0,0 +1,25 @@
+<footer class="page-section bg-gray-lighter footer pb-60">
+    <div class="container">
+        <div class="local-scroll mb-30 wow fadeInUp" data-wow-duration="1.5s">
+            <a href="index.php"><img src="images/logo-footer.png" width="178" height="36" alt="musica bodas granada" /></a>
+        </div>
+        <!-- <div class="footer-social-links mb-110 mb-xs-60">
+            <a href="galeria.php" title="Facebook"><i class="fa fa-facebook"></i></a>
+            <a href="sobrenosotros.php" title="Twitter"><i class="fa fa-twitter"></i></a>
+            <a href="repertorio.php" title="Behance"><i class="fa fa-behance"></i></a>
+            <a href="contacto.php" title="LinkedIn+"><i class="fa fa-linkedin"></i></a>
+            <a href="videos.php" title="Pinterest"><i class="fa fa-pinterest"></i></a>
+        </div> -->
+        <div class="footer-text">
+            <div class="footer-copy font-alt">
+                <a >&copy; Dúo Belcanto 2015</a>.
+            </div>
+            <div class="footer-made">
+                Música Bodas Granada.
+            </div>
+        </div>
+     </div>
+     <div class="local-scroll">
+         <a href="#top" class="link-to-top"><i class="fa fa-caret-up"></i></a>
+     </div>
+</footer>
---
+++ inc/nav.php
@@ -0,0 +1,41 @@
+<nav class="main-nav js-stick">
+    <div class="full-wrapper relative clearfix">
+        <div class="nav-logo-wrap local-scroll">
+            <a href="index.php" class="logo">
+                <img src="images/logo-dark.png" alt="musicos para bodas en Almeria y Granada" width="202" height="96"/>
+            </a>
+        </div>
+        <div class="mobile-nav">
+            <i class="fa fa-bars"></i>
+        </div>
+        
+        <div class="inner-nav desktop-nav">
+            <ul class="clearlist">
+                
+                <li>
+                    <a href="index.php" >Inicio</a>
+                </li>
+                <!-- <li>
+                    <a href="sobrenosotros.php">Sobre Nosotros</a>
+                </li> -->
+                <li>
+                    <a href="repertorio.php">Repertorio</a>
+                </li>
+                <!-- <li>
+                    <a href="galeria.php">Galería</a>
+                </li> -->
+                <li>
+                    <a href="videos.php">Videos</a>
+                </li>
+                <li>
+                    <a href="contacto.php">Contacto</a>
+                </li>
+                <!-- <li><a>&nbsp;</a></li>
+                <li><a>&nbsp;</a></li>
+                <li><a>&nbsp;</a></li> -->
+                
+            </ul>
+        </div>
+    </div>
+</nav>  
+     
